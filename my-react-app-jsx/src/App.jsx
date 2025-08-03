13be9c0826a956ab17fdee95063a86c8721ac92e
Artem Neij <artemneij@gmail.com>
2025-08-03 13:04:10 +0300

    first

---
+++ my-react-app-jsx/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <blockquote>
+        <h3>"Настоящая любовь начинается там, где ничего не ждут взамен."</h3>
+        <footer>— Антуан де Сент-Экзюпери ©</footer>
+      </blockquote>
+    </>
+  )
+}
+
+export default App
+
